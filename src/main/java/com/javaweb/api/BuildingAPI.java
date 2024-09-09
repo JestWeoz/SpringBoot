5ac1f6ae7ad0cde5332e07095e30006f2ab47049
JestWeoz <tuannguyenquoc057@gmail.com>
2024-09-09 23:57:29 +0700

    etst

--- src/main/java/com/javaweb/api/BuildingAPI.java
+++ src/main/java/com/javaweb/api/BuildingAPI.java
@@ -2,9 +2,13 @@ package com.javaweb.api;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BuildingAPI {
-
+    @GetMapping (value = "/api/building/")
+    public void building(@RequestParam (value = "name") String name) {
+        System.out.println(name);
+    }
 }
