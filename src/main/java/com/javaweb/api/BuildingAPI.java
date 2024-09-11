2151420f1048ed4e8f2eadf24a650c3ab6de4548
JestWeoz <tuannguyenquoc057@gmail.com>
2024-09-11 15:28:22 +0700

    test

--- src/main/java/com/javaweb/api/BuildingAPI.java
+++ src/main/java/com/javaweb/api/BuildingAPI.java
@@ -1,12 +1,22 @@
 package com.javaweb.api;
 
 
+import com.javaweb.model.BuildingDTO;
+import com.javaweb.service.BuildingService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class BuildingAPI {
+    @Autowired
+    private BuildingService buildingService;
+
     @GetMapping(value="/api/building/")
-    public void building(@RequestParam(name = "name") String name) {
-        System.out.println(name);
+    public List<BuildingDTO> building(@RequestParam(name = "name") String name,
+                                      @RequestParam(name = "districtid") Long districtid) {
+        List<BuildingDTO> ds = buildingService.findAll(name, districtid);
+        return ds;
     }
 }
